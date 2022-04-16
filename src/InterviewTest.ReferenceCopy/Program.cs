e2c53922eefd4a30a0892ff9591e73bdee076592
Daniel Dieppa <ddieppa@junipercommerce.com>
2022-04-15 23:49:36 -0400

    Reference tests

---
+++ src/InterviewTest.ReferenceCopy/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+var sb = new StringBuilder();
+var sbCopy = sb;
+sb.Append("Hello World!");
+sb = null;
+
+Console.WriteLine(sbCopy.ToString());
